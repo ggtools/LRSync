7149c3e5f925a61185cfc0debfd972c40ad698ba
Christophe Labouisse <christophe@labouisse.org>
2012-01-30 00:31:50 +0100

    Rewriting of LRSync initialization and option loading. Closes #10

---
+++ lib/init.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+#
+# This file is part of LRSync. Copyright © 2011-2012 Christophe Labouisse.
+# 
+# LRSync is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# LRSync is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with LRSync.  If not, see <http://www.gnu.org/licenses/>.
+
+# Reads a section of the ini file and put the results in variables prefixed by the supplied prefix. 
+readIniSection() {
+	local PREFIX="$1"
+	local SECTION="${2-main}"
+	
+	# Neat way to parse the ini section from LRS_INI
+	# shamelessly taken from http://www.tuxz.net/blog/
+	eval `sed -e 's/[[:space:]]*\=[[:space:]]*/=/g' \
+    -e 's/;.*$//' \
+    -e 's/[[:space:]]*$//' \
+    -e 's/^[[:space:]]*//' \
+    -e "s/^\(.*\)=\([^\"']*\)$/\1=\"\2\"/" \
+    -e "s/^\(.*\)=/${PREFIX}\1=/" \
+   < $LRS_INI \
+    | sed -n -e "/^\[$SECTION\]/,/^\s*\[/{/^[^;].*\=.*/p;}"`
+}
+
+usage() {
+	cat <<EOF
+lrsync -c catalog -d direction [-q] [-r repo_dir]
+
+	-c catalog  : the catalog to be converted, must be declared in lrsync.ini
+	-d direction: direction of the conversion either fromRepo or toRepo
+	-q          : remove output during conversion
+	-r repo_dir : directory containing the reference catalogs
+	
+	
+EOF
+}
+
+msg() {
+	[ "$LRS_QUIET" ] || echo $@
+}
+
+LRS_INIDIR="$HOME/.lrsync"
+LRS_INI="$LRS_INIDIR/lrsync.ini"
+SQLITE="sqlite3"
+
+# Create the lrsync configuration directory and copy a default .ini file.
+if [ ! -d "$LRS_INIDIR" ]; then
+	mkdir -p "$LRS_INIDIR"
+	cp "$LRS_LIBDIR/default.ini" "$LRS_INI"
+fi
+
+# Load the "main" section of LRS_INI
+readIniSection LRS_
+
+# Override it with the command line
+while getopts "c:d:qr:" opt; do
+	case $opt in
+		c)
+			LRS_CATALOG="$OPTARG"
+			;;
+		d)
+			if [[ "$OPTARG" =~ ^from|to ]]; then
+				LRS_DIRECTION=${BASH_REMATCH[0]}
+			else
+				echo "Invalid direction '$OPTARG'" >&2
+				exit 1
+			fi
+			;;
+		q)
+			LRS_QUIET=1
+			;;
+		r)
+			LRS_REPODIR="$OPTARG"
+			;;
+		\?)
+			[ -n "$OPTARG" ] && echo "Invalid option: -$OPTARG" >&2
+			usage >&2
+			exit 1
+			;;
+		esac
+done
+
+mkdir -p "$LRS_REPODIR"
+
+if [ ! -d "$LRS_REPODIR" ]; then
+	echo "Repository directory '$LRS_REPODIR' does not exist" >&2
+	exit 2
+fi
+
+if [ -z "$LRS_DIRECTION" ]; then
+	echo "-d direction is mandatory" >&2
+	usage >&2
+	exit 2
+fi
+
+if [ -z "$LRS_CATALOG" ]; then
+	echo "-c catalog is mandatory" >&2
+	usage >&2
+	exit 2
+fi
+
+readIniSection LRS_CAT_ "$LRS_CATALOG"
+
+if [ ! -d "$LRS_CAT_DIR" ]; then
+	echo "Catalog directory '$LRS_CAT_DIR' not found" >&2
+	usage >&2
+	exit 3
+fi
+
+LRS_CAT_FILE="$LRS_CAT_DIR/$LRS_CATALOG.lrcat"
+if [ "$LRS_DIRECTION" = "to" -a ! -f "$LRS_CAT_FILE" ]; then
+	echo "Catalog file '$LRS_CAT_FILE' not found" >&2
+	usage >&2
+	exit 3
+fi
+
+LRS_REPO_FILE="$LRS_REPODIR/$LRS_CATALOG.lrcat"
+if [ "$LRS_DIRECTION" = "from" -a ! -f "$LRS_REPO_FILE" ]; then
+	echo "Repo file '$LRS_REPO_FILE' not found" >&2
+	usage >&2
+	exit 3
+fi
+
+[[ "$LRS_CAT_FOLDERCONFIG" =~ ^/ ]] || LRS_CAT_FOLDERCONFIG="$LRS_INIDIR/$LRS_CAT_FOLDERCONFIG"
+
+if [ ! -f "$LRS_CAT_FOLDERCONFIG" ]; then
+	echo "Catalog '$LRS_CATALOG' is not properly defined in lrsync.ini." >&2
+	usage >&2
+	exit 3
+fi
+
+# Loading volume definitions
+msg "Loading  folder configuration from '$LRS_CAT_FOLDERCONFIG'"
+i=0
+for a in $(sed -n '{s/#.*//; /=/p;}' "$LRS_CAT_FOLDERCONFIG"); do
+	repoVols[$i]=$(echo "$a" | cut -d = -f 1)
+	catVols[$i]=$(echo "$a" | cut -d = -f 2)
+	let i++
+done
+
+if [ -z "$LRS_QUIET" ]; then
+	cat <<EOF
+Catalog   : $LRS_CATALOG
+Direction : $LRS_DIRECTION
+Repository: $LRS_REPODIR
+Repo file : $LRS_REPO_FILE
+LRCat dir : $LRS_CAT_DIR
+LRCat file: $LRS_CAT_FILE
+Folders:
+EOF
+	for (( i=0 ; i < ${#repoVols[*]} ; i++ ))
+	do
+		echo "            ${repoVols[$i]} -> ${catVols[$i]}"
+	done
+	echo ""
+fi
